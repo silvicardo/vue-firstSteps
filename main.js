f5bb16a659f05d48ff5a5d25ac12f25a92646320
Riccardo Silvi <silvicardo@gmail.com>
2019-03-12 08:25:36 +0100

    components and slots

--- main.js
+++ main.js
@@ -1,6 +1,46 @@
 
-//Linkare il container principale nella pagina
+//Componenti
 
+Vue.component('school',{
+  template: `<h1>Boolean main topics are:</h1>`
+})
+
+
+//COMPONENTE TOPICS
+//Questo componente passa dei dati a topic
+Vue.component('topics', {
+  template: `<div>
+                <ul>
+                  <topic v-for='topic in topics' :key='topic.id'>{{ topic.name }}</topic>
+                </ul>
+              </div>
+            `,
+  data() {
+    return {
+      topics:[
+        {id: 1, name: 'PHP'},
+        {id: 2, name: 'JAVASCRIPT'},
+        {id: 3, name: 'LARAVEL'},
+        {id: 4, name: 'VUE'},
+        {id: 5, name: 'JQUERY'}
+      ]
+    }
+  }
+
+})
+
+//COMPONENTE TOPIC SINGOLO
+//i tag slot mostrano il dato immesso all'interno di questo componente
+//richiamato dal padre
+Vue.component('topic', {
+  template: `
+            <li><slot></slot></li>
+              `
+})
+
+
+
+//Linkare il container principale nella pagina
 
 const app = new Vue({
   el: '#app', //l'elemento linkato
